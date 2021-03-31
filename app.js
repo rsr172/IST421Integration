20ca044d03755b9f80e9246a53f43a8601d40a2a
Dp05240 <63268243+Dp05240@users.noreply.github.com>
2021-03-31 11:29:34 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+function searchCars () {
+    var x = document.getElementById("CarMake").value;
+    var y = document.getElementById("CarYear").value;
 
+    $.getJSON('https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/' + x + '/modelyear/' + y + '?format=json', function(data) {
+        console.log(data); 
+        var z  = JSON.stringify(data);
+        console.log(z);
+    });
+
+  }
